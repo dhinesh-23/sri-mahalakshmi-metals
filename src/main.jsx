51bdc6003dcd41ab1e38c16745337f1031cfd280
dhinesh-23 <sankarsnr123@gmail.com>
2025-12-31 17:03:17 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route ,Routes} from 'react-router-dom'
-import'./Style.css'
+import "./style.css";
 import Home from './Home'
 
 createRoot(document.getElementById('root')).render(
