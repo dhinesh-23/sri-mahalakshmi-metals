499f8b20bd9a6c9e57aaaee17224509850c25373
dhinesh-23 <sankarsnr123@gmail.com>
2026-01-06 14:46:26 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route ,Routes} from 'react-router-dom'
 import "./style.css";
+import Aboutpage from './aboutpage';
 import Home from './Home'
 import Collectionpage from './collectionpage';
-import Aboutpage from './aboutpage';
-
 
 
 
